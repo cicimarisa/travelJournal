003a4ddf4c3b78efa4395c040d7928ff1f3d3923
Cici Marisa Hardianti <cicimarisahardianti@gmail.com>
2022-09-07 07:16:54 +0800

    Update
    Update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,13 +4,13 @@ export default function Card(props){
     return (
         <div className="card">
             <img src={props.imageUrl} alt="" className="card--image"/>
-            <div>
+            <div className="card--location">
                 <img src={location} alt="" className="location--icon"/>
                 <p className="location">{props.location}</p>
                 <a href={props.googleMapsUrl} className="location--link" target="_blank">View on Google Maps</a>
                 <h1 className="location--title">{props.title}</h1>
                 <p className="location--visit-duration">{props.startDate} - {props.endDate}</p>
-                <p className="description">{props.description}</p>
+                <p className="location--description">{props.description}</p>
             </div>
         </div>
     )
